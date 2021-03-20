0a6ea3e1fcdf928b858726d7deda4e4923e162a2
島崎晃次 <shimazakikoji@shimazakikojiMacBook-Air.local>
2021-03-20 16:38:30 +0900

    新規登録画面への遷移

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :email, :first_name, :last_name, :firse_name_kana, :last_name_kana, :birthday)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :users, only: :show
+  resources :users, only: [:edit, :update, :destroy]
 end
