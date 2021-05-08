6694e66f3933ae22df6e2d4fc4b7407d0ee24e64
島崎晃次 <shimazakikoji@shimazakikojiMacBook-Air.local>
2021-05-08 14:29:21 +0900

    テストコードの実装

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     address { 'fuga1-1-1'}
     building { 'hogefuga'}
     phone_number { "09000000000" }
+    token { "tok_abcdefghijk00000000000000000" }
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '配送先の保存' do
     before do
-      @order_address = FactoryBot.build(:order_address)
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+      sleep(1)
     end
 
     it '値が正しく入力されていれば保存できること' do
@@ -13,9 +16,15 @@ RSpec.describe OrderAddress, type: :model do
     it '郵便番号が空だと保存できない' do
       @order_address.postal_code = ""
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Postal code can't be blank", "Postal code is invalid. Include hyphen(-)")
+      expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
     end
     
+    it '郵便番号にハイフンが含まれていないと保存できない' do
+      @order_address.postal_code = "9999999"
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+    end
+
     it '都道府県が空だと保存できない' do
       @order_address.shipment_id = ''
       @order_address.valid?
@@ -38,6 +47,12 @@ RSpec.describe OrderAddress, type: :model do
       @order_address.phone_number = ""
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+    end
+
+    it '電話番号にハイフンが含まれていると保存できない' do
+      @order_address.phone_number = "090-1234-5678"
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number is invalid")
     end
   end
 end
