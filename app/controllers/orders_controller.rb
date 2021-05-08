0b1d4ac58225462a9252ccc84ebffbadac9a972d
島崎晃次 <shimazakikoji@shimazakikojiMacBook-Air.local>
2021-05-08 13:40:09 +0900

    商品詳細ページにて売切れ商品の表示の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,17 @@
 class OrdersController < ApplicationController
-  before_action :item.find only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
-    item.find
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-    item.find
     if @order_address.valid? 
       pay_item
       @order_address.save
-      redirect_to root_path
+      return redirect_to root_path
     else
       render 'index'
     end
@@ -24,9 +23,6 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :shipment_id, :city, :address, :building, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
-
-  def item.find
-    @item = Item.find(params[:item_id])
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
