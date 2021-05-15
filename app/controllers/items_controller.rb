8cd796a00b7714a8a1916e85939947666ecbae56
島崎晃次 <shimazakikoji@shimazakikojiMacBook-Air.local>
2021-05-15 15:27:05 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy, :show]
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy, :show]
 
   def index
