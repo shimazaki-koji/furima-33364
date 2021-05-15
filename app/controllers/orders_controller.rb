99917d53e95d415cb321d00300e2df60c817833e
島崎晃次 <shimazakikoji@shimazakikojiMacBook-Air.local>
2021-05-15 19:12:03 +0900

    修正6

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,9 +31,12 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    unless @item.user
+    unless current_user.id == @item.user
       redirect_to root_path
     end
+    #unless @item.user
+      #redirect_to root_path
+    #end
   end
 
   def move_to_order
